dddf409e1a19cde54eb107a6700d8ce76e1c3c30
William Nascimento <williamnnmiranda@gmail.com>
2023-03-05 21:10:59 -0300

    feat: :sparkles: added service login

--- src/services/User.ts
+++ src/services/User.ts
@@ -1,7 +1,8 @@
+import { api } from '../helpers/api';
 import { UserLogin } from './../interfaces/user_interfaces';
 const userServices = {
   login : async (data : UserLogin) => {
-    
+    const user = await (await api.post('/access/login')).data
   }
 }
 

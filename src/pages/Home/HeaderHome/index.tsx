de6dd59e5d42c29fbc5453d1b69762ecad415cce
William Nascimento <williamnnmiranda@gmail.com>
2023-03-14 11:36:38 -0300

    feat: :sparkles: header scope created

--- src/pages/Home/HeaderHome/index.tsx
+++ src/pages/Home/HeaderHome/index.tsx
@@ -1,10 +1,37 @@
-import React from "react";
+import * as C from "./style";
+import iconSvg from "../../../assets/config.svg";
+import iconShare from "../../../assets/share.svg";
 import { IInfos } from "../../../interfaces/collections_interface";
 interface IProps {
-  infos: IInfos
+  infos: IInfos;
 }
 const HeaderHome = ({ infos }: IProps) => {
-  return <div>{infos.name}</div>;
+  return (
+    <C.ContainerHeaderInfo>
+      <C.LogoInfo></C.LogoInfo>
+      <C.ContainerInfos>
+        <C.NameCollection>
+          {infos.abbreviation} - {infos.name}
+        </C.NameCollection>
+        <C.TextOfCompletedAndIncompleteTasks>
+          {`${infos.completedCards} de ${infos.totalTasksInCards} cards completos`}
+        </C.TextOfCompletedAndIncompleteTasks>
+        <C.ContainerOptionsInfos>
+          <C.Icon src={iconSvg} />
+          <C.ButtonStart>Estudar</C.ButtonStart>
+          <C.Share>
+            <C.Icon src={iconShare} /> Compartilhar
+          </C.Share>
+          <C.MoreOptions>
+            <C.MoreOptionIcon />
+            <C.MoreOptionIcon />
+            <C.MoreOptionIcon />
+          </C.MoreOptions>
+        </C.ContainerOptionsInfos>
+      </C.ContainerInfos>
+      <C.ContainerGraphic> </C.ContainerGraphic>
+    </C.ContainerHeaderInfo>
+  );
 };
 
 export default HeaderHome;
---
+++ src/pages/Home/HeaderHome/style.ts
@@ -0,0 +1,36 @@
+import styled from "styled-components";
+
+export const ContainerHeaderInfo = styled.section`
+
+`
+export const LogoInfo = styled.section`
+
+`
+export const ContainerInfos = styled.section`
+
+`
+export const ContainerGraphic = styled.section`
+
+`
+export const NameCollection = styled.div`
+  text-transform: uppercase;
+`
+export const TextOfCompletedAndIncompleteTasks = styled.p``
+
+export const Icon = styled.img`
+  width: 20px;
+  height: 20px;
+`;
+
+export const ContainerOptionsInfos = styled.div``
+
+export const ButtonStart = styled.button``
+export const Share = styled.div``
+export const MoreOptions = styled.div``
+
+export const MoreOptionIcon = styled.div`
+  width: 7px;
+  height: 7px;
+  background-color: grey;
+  border-radius: 50%;
+`

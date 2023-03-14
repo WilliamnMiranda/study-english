929c34c3b8d7ac0df78e7fa15ff9bdc69cce6c09
William Nascimento <williamnnmiranda@gmail.com>
2023-03-14 16:53:30 -0300

    feat: :sparkles: added component loading check

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -6,7 +6,7 @@ import HeaderHome from "./HeaderHome";
 
 const Home = () => {
   const { activeCollection } = useContext(CollectionContext);
-  const [info, setInfo] = useState<IInfos>({} as IInfos);
+  const [info, setInfo] = useState<IInfos | null>(null);
   const getCollection = async () => {
     const info = await collectionServices.getInfo(activeCollection);
     setInfo(info);
@@ -15,9 +15,9 @@ const Home = () => {
     if (activeCollection) getCollection();
   }, [activeCollection]);
   return (
-    <div>
+    info ? ( <div>
       <HeaderHome infos={info} />
-    </div>
+    </div>) : <div>loading</div>
   );
 };
 

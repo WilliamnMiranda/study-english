b606812a37f753c08a4462fe8203276b92f24fb7
william nascimento <williamnnmiranda@gmail.com>
2023-03-23 13:01:02 -0300

    feat: :sparkles: Removed bug of not opening modal and when there are no collections, it crashes the application

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -4,15 +4,18 @@ import { CollectionContext } from "../../contexts/Collection_Context";
 import { ICollection, IInfos } from "../../interfaces/collections_interface";
 import collectionServices from "../../services/Collection";
 import decksServices from "../../services/Decks";
+import EmptyImage from '../../assets/empty.svg'
 import Decks from "./Decks";
 import HeaderHome from "./HeaderHome";
 import LoadingImage from '../../assets/loading.gif'
 import Modal from "../../components/modal";
 import { ModalContext } from "../../contexts/Modal_Context";
+import * as C from './style'
 const Home = () => {
   const { activeCollection } = useContext(CollectionContext);
+  const { modal } = useContext(ModalContext)
   const [info, setInfo] = useState<IInfos | null>(null);
-
+  console.log(activeCollection)
   const mutateDeck = useMutation(() => decksServices.getAll(activeCollection), {
     onSuccess: (data) => {
       console.log(data)
@@ -39,16 +42,31 @@ const Home = () => {
     }
   }, [activeCollection]);
   console.log(decks)
-  const { modal } = useContext(ModalContext)
+
+
+
+  const EmptyCards = () => {
+    return (
+      <C.ContainerEmpty>
+        <img src={EmptyImage} />
+        <p>Voce nao possui nenhum cartao criado</p>
+        <button> Criar um Deck</button>
+      </C.ContainerEmpty>
+    )
+  }
   return (
     <>
       {modal.isOpen && <Modal />}
       {
-        mutateDeck !== undefined && info &&
-        <div>
-          <HeaderHome infos={info} />
-          <Decks decks={decks} />
-        </div>
+        activeCollection === '' ? <EmptyCards /> : <>
+          {
+            mutateDeck !== undefined && info &&
+            <div>
+              <HeaderHome infos={info} />
+              <Decks decks={decks} />
+            </div>
+          }
+        </>
       }
     </>
   );

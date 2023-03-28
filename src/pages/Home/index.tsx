6afc534a8406d6cb8c79cdd7d8615b143c7b9cdd
william nascimento <williamnnmiranda@gmail.com>
2023-03-28 20:18:22 -0300

    refactor: :recycle: fixed and removed unnecessary imports in the home

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,18 +1,19 @@
 import { useMutation, useQuery } from "@tanstack/react-query";
-import React, { useContext, useEffect, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import { CollectionContext } from "../../contexts/Collection_Context";
-import { ICollection, IInfos } from "../../interfaces/collections_interface";
+import { IInfos } from "../../interfaces/collections_interface";
+import { BeatLoader } from "react-spinners";
+import { ModalContext } from "../../contexts/Modal_Context";
+//default imports
 import collectionServices from "../../services/Collection";
 import decksServices from "../../services/Decks";
-import EmptyImage from '../../assets/empty.svg'
 import Decks from "./Decks";
 import HeaderHome from "./HeaderHome";
 import Collections from '../../assets/Collections.svg'
 import Modal from "../../components/modal";
-import { ModalContext } from "../../contexts/Modal_Context";
 import * as C from './style'
-import Loading from "../../components/loading";
-import { BeatLoader } from "react-spinners";
+
+
 const Home = () => {
   const { activeCollection } = useContext(CollectionContext);
   const { modal } = useContext(ModalContext)

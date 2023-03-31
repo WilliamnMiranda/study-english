6dd2e8b7270fe99bb0ff38717bcb77703c5b29a7
william nascimento <williamnnmiranda@gmail.com>
2023-03-31 14:50:57 -0300

    refactor: :recycle: refactored the header refactored for better performance

--- src/contexts/Collection_Context.tsx
+++ src/contexts/Collection_Context.tsx
@@ -45,7 +45,8 @@ export const CollectionProvider = ({ children }: IProps) => {
   }
 
   useEffect(() => {
-    getInfo()
+    if (activeCollection)
+      getInfo()
   }, [activeCollection])
 
   useEffect(() => {
--- src/pages/Home/HeaderHome/index.tsx
+++ src/pages/Home/HeaderHome/index.tsx
@@ -13,10 +13,47 @@ import { CollectionContext } from "../../../contexts/Collection_Context";
 
 const HeaderHome = () => {
   const { infoCollection } = useContext(CollectionContext)
-  if (infoCollection == null) console.log(infoCollection)
   const { setTypeModal } = useContext(ModalContext)
+  const percentage = infoCollection?.completedCards ? infoCollection?.totalTasksInCards ? Math.floor(infoCollection?.completedCards / infoCollection?.totalTasksInCards * 100) : 0 : 0
   return (
-    <div> a </div>
+    <C.ContainerHeaderInfo>
+      <C.ContainerInfos>
+        <C.LogoInfo>{infoCollection?.name[0]}</C.LogoInfo>
+        <C.ContainerAllInfos>
+          <C.NameCollection>
+            {infoCollection?.abbreviation} - {infoCollection?.name} <C.Icon src={iconEdit} />
+          </C.NameCollection>
+          <C.TextOfCompletedAndIncompleteTasks>
+            <C.TextBoldItem>{infoCollection?.completedCards} </C.TextBoldItem>
+            de
+            <C.TextBoldItem> {infoCollection?.totalTasksInCards} </C.TextBoldItem>
+            cards concluidos
+          </C.TextOfCompletedAndIncompleteTasks>
+          <C.ContainerOptionsInfos>
+            <C.Icon src={iconSvg} />
+            <C.ButtonStart>Estudar</C.ButtonStart>
+            <C.AddDeck>
+              <AiOutlinePlus onClick={() => setTypeModal('deck', 'open')} />
+            </C.AddDeck>
+            <C.Share>
+              <C.Icon src={iconShare} /> Compartilhar
+            </C.Share>
+            <C.MoreOptions>
+              <C.MoreOptionIcon />
+              <C.MoreOptionIcon />
+              <C.MoreOptionIcon />
+            </C.MoreOptions>
+          </C.ContainerOptionsInfos>
+        </C.ContainerAllInfos>
+      </C.ContainerInfos>
+      <C.ContainerGraphic>
+        <CircularProgressbar
+          strokeWidth={10}
+          styles={
+            { root: { height: '110px' } }
+          } value={percentage} maxValue={100} text={`${percentage}%`} />
+      </C.ContainerGraphic>
+    </C.ContainerHeaderInfo>
   );
 };
 
